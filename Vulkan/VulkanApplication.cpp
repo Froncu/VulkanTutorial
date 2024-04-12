68643aa74ed4f40e18059fc990cef66539bf9353
Froncu <jakubfratczak204@gmail.com>
2024-04-13 00:43:06 +0200

    [FEATURE] include instance validation layers in the VkInstance & add support for checking their availability

--- Vulkan/VulkanApplication.cpp
+++ Vulkan/VulkanApplication.cpp
@@ -44,8 +44,40 @@ bool isExtensionAvailable(std::string_view extensionName)
 	);
 };
 
+std::vector<VkLayerProperties> getAvailableValidationLayers()
+{
+	std::uint32_t availableValidationLayersCount;
+	vkEnumerateInstanceLayerProperties(&availableValidationLayersCount, nullptr);
+	std::vector<VkLayerProperties> vAvaialbleValidationLayers(availableValidationLayersCount);
+	vkEnumerateInstanceLayerProperties(&availableValidationLayersCount, vAvaialbleValidationLayers.data());
+
+	return vAvaialbleValidationLayers;
+}
+
+bool isValidationLayerAvailable(std::string_view validationLayerName)
+{
+	const std::vector<VkLayerProperties> vAvaialbleValidationLayers{ getAvailableValidationLayers() };
+
+	return std::any_of
+	(
+		vAvaialbleValidationLayers.begin(), vAvaialbleValidationLayers.end(),
+		[validationLayerName](const VkLayerProperties& availableValidationLayer)
+		{
+			return validationLayerName == availableValidationLayer.layerName;
+		}
+	);
+};
+
 [[nodiscard("handle to created instance ignored!")]] VkInstance createInstance()
 {
+#ifndef NDEBUG
+	const std::vector vRequiredValidationLayerNames{ "VK_LAYER_KHRONOS_validation" };
+
+	for (std::string_view requiredValidationLayerName : vRequiredValidationLayerNames)
+		if (!isValidationLayerAvailable(requiredValidationLayerName))
+			throw std::runtime_error(std::format("validation layer {} is not available!", requiredValidationLayerName));
+#endif
+
 	std::uint32_t requiredExtensionCount;
 	char const* const* const ppRequiredExtensionNames{ glfwGetRequiredInstanceExtensions(&requiredExtensionCount) };
 
@@ -57,6 +89,10 @@ bool isExtensionAvailable(std::string_view extensionName)
 	VkInstanceCreateInfo const instanceCreateInfo
 	{
 		.sType{ VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO },
+#ifndef NDEBUG
+		.enabledLayerCount{ static_cast<uint32_t>(vRequiredValidationLayerNames.size()) },
+		.ppEnabledLayerNames{ vRequiredValidationLayerNames.data() },
+#endif
 		.enabledExtensionCount{ requiredExtensionCount },
 		.ppEnabledExtensionNames{ ppRequiredExtensionNames }
 	};
